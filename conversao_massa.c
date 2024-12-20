6bf42ea20f9c66ef2272d64347651467608e2da5
Gustavo Dimas <gustavodimas.92@gmail.com>
2024-12-19 21:11:23 -0300

    Segunda versão

--- conversao_massa.c
+++ conversao_massa.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-#include <string.h>
 
 void converterMassa() {
     double valor;
-    char unidadeOrigem[10], unidadeDestino[10];
+    int unidadeOrigem, unidadeDestino;
 
     printf("=== Conversao de Massa ===\n");
-    printf("Unidades disponiveis: kg, g, t\n");
+    printf("Unidades disponiveis:\n");
+    printf("1 - kg\n");
+    printf("2 - g\n");
+    printf("3 - t\n");
+    
     printf("Digite o valor da massa: ");
     if (scanf("%lf", &valor) != 1) {
         printf("Entrada invalida. Certifique-se de inserir um numero.\n");
@@ -18,51 +21,66 @@ void converterMassa() {
         return;
     }
 
-    printf("Digite a unidade de origem (kg/g/t): ");
-    scanf("%s", unidadeOrigem);
+    printf("Digite o numero da unidade de origem (1-kg, 2-g, 3-t): ");
+    if (scanf("%d", &unidadeOrigem) != 1) {
+        printf("Entrada invalida para unidade de origem.\n");
+        return;
+    }
 
-    //unidade de origem
-    if (strcmp(unidadeOrigem, "kg") != 0 && 
-        strcmp(unidadeOrigem, "g") != 0 && 
-        strcmp(unidadeOrigem, "t") != 0) {
+    if (unidadeOrigem < 1 || unidadeOrigem > 3) {
         printf("Unidade de origem invalida.\n");
         return;
     }
 
-    printf("Digite a unidade de destino (kg/g/t): ");
-    scanf("%s", unidadeDestino);
-
-    //unidade de destino
-    if (strcmp(unidadeDestino, "kg") != 0 && 
-        strcmp(unidadeDestino, "g") != 0 && 
-        strcmp(unidadeDestino, "t") != 0) {
-        printf("Unidade de destino invalida.\n");
+    printf("Digite o numero da unidade de destino (1-kg, 2-g, 3-t): ");
+    if (scanf("%d", &unidadeDestino) != 1) {
+        printf("Entrada invalida para unidade de destino.\n");
         return;
     }
 
-    //conversões:
-    //1 kg = 1000 g
-    //1 t = 1000 kg = 1.000.000 g
+    if (unidadeDestino < 1 || unidadeDestino > 3) {
+        printf("Unidade de destino invalida.\n");
+        return;
+    }
     double valorEmKg;
 
-    //valor de origem para kg
-    if (strcmp(unidadeOrigem, "kg") == 0) {
-        valorEmKg = valor;
-    } else if (strcmp(unidadeOrigem, "g") == 0) {
-        valorEmKg = valor / 1000.0;
-    } else { // unidadeOrigem == "t"
-        valorEmKg = valor * 1000.0;
+    switch (unidadeOrigem) {
+        case 1:
+            valorEmKg = valor;
+            break;
+        case 2:
+            valorEmKg = valor / 1000.0;
+            break;
+        case 3:
+            valorEmKg = valor * 1000.0;
+            break;
+        default:
+            printf("Unidade de origem invalida.\n");
+            return;
     }
 
-    // Converte de kg para unidade destino
     double valorConvertido;
-    if (strcmp(unidadeDestino, "kg") == 0) {
-        valorConvertido = valorEmKg;
-    } else if (strcmp(unidadeDestino, "g") == 0) {
-        valorConvertido = valorEmKg * 1000.0;
-    } else { // unidadeDestino == "t"
-        valorConvertido = valorEmKg / 1000.0;
+    switch (unidadeDestino) {
+        case 1:
+            valorConvertido = valorEmKg;
+            break;
+        case 2:
+            valorConvertido = valorEmKg * 1000.0;
+            break;
+        case 3:
+            valorConvertido = valorEmKg / 1000.0;
+            break;
+        default:
+            printf("Unidade de destino invalida.\n");
+            return;
+    }
+    char *nomeUnidadeDestino;
+    switch (unidadeDestino) {
+        case 1: nomeUnidadeDestino = "kg"; break;
+        case 2: nomeUnidadeDestino = "g";  break;
+        case 3: nomeUnidadeDestino = "t";  break;
+        default: nomeUnidadeDestino = "";   break;
     }
 
-    printf("Resultado: %.6f %s\n", valorConvertido, unidadeDestino);
+    printf("Resultado: %.6f %s\n", valorConvertido, nomeUnidadeDestino);
 }
