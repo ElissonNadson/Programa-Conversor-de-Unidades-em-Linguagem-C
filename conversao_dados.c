ddb4443dcd3d88c25bf740a3a9370875cb314b29
Elisson Marques <nadsonnodachi@gmail.com>
2024-12-23 00:58:43 -0300

    Update conversao_dados.c

--- conversao_dados.c
+++ conversao_dados.c
@@ -1,7 +1,7 @@
+// Bits, bytes, kilobytes (KB), megabytes (MB), gigabytes (GB), terabytes (TB)
 #include <stdio.h>
 #include <stdlib.h>
 
-// Bits, bytes, kilobytes (KB), megabytes (MB), gigabytes (GB), terabytes (TB)
 // Função para conversão de dados
 double calcularConversarDados(double valor, int deUnidade, int paraUnidade) {
     double unidades[] = {1, 8, 8 * 1024, 8 * 1024 * 1024, 8 * 1024 * 1024 * 1024, 8 * 1024 * 1024 * 1024 * 1024};
